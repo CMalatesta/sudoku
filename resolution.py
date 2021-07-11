84ec2cf6e8b494abfbdcccec73eb109072cdde2c
Cami Malatesta <camilamalatesta.exactas@gmail.com>
2021-07-11 01:14:36 -0300

    Add solve function to resolution.py

--- resolution.py
+++ resolution.py
@@ -26,29 +26,36 @@ def filter_uniques(candidates: np.ndarray) -> List[Tuple[int, int, int]]:
     return filter
 
 
-box_side = 3
+def solve(input_sudoku: np.ndarray, box_side: int) -> np.ndarray:
+    sudoku = np.copy(input_sudoku)
+    while 0 in sudoku:
+        candidates = get_candidates(sudoku, box_side)
+        filtered = filter_uniques(candidates)
+        if not filtered:
+            print("There aren't uniques values")
+            break
+        for row, column, value in filtered:
+            sudoku[row, column] = value
+    return sudoku
 
-original_sudoku = np.array([
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-])
 
-sudoku = np.copy(original_sudoku)
+if __name__ == "__main__":
+    box_side = 3
 
-while 0 in sudoku:
-    candidates = get_candidates(sudoku, box_side)
-    filtered = filter_uniques(candidates)
-    if not filtered:
-        print("There aren't uniques values")
-        break
-    for row, column, value in filtered:
-        sudoku[row, column] = value
+    original_sudoku = np.array([
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ])
 
-print(sudoku)
+    sudoku = np.copy(original_sudoku)
+
+    solve_sudoku = solve(sudoku, box_side)
+    print(sudoku)
+    print(original_sudoku[original_sudoku > 0].size)
